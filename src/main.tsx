ff13c832ea1fdfa9506c0077534805d0824836d2
saurabh chauhan <sourabh1202@gmail.com>
2026-02-01 20:30:59 +0200

    basic useState usage

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './BasicUseState.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
